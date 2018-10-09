d2f2d2dd25c5edcc2fc8cf65d0a9493c3365da7c
fiahfy <d.fiahfy@gmail.com>
2018-10-09 09:39:16 +0900

    Update configs

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import { buildText } from '~/utils/accelerator'
+import electronAcceleratorFormatter from '@fiahfy/electron-accelerator-formatter'
 
 Vue.use(Vuetify, {
   theme: {
@@ -10,7 +10,7 @@ Vue.use(Vuetify, {
 })
 
 Vue.filter('accelerator', (title, accelerator) => {
-  return `${title} (${buildText(accelerator)})`
+  return `${title} (${electronAcceleratorFormatter(accelerator)})`
 })
 
 Vue.filter('readableSize', (bytes) => {
